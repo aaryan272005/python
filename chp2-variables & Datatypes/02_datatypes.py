76dc74405c79804c0af4ed0349c5d8dd843cf25a
aaryan272005 <aaryan24398@holycrosskurla.com>
2025-02-09 15:05:52 +0530

    .

--- chp2-variables & Datatypes/02_datatypes.py
+++ chp2-variables & Datatypes/02_datatypes.py
@@ -1,13 +1,14 @@
-a=2 # a is a interger value
-
-b=2.42 # b is a floating point number
-
-c="Aaryan"
-# c is a sting value /should be in "aryan"
-
-d=True
-# d is a boolean value /note : T and F should be capital
- 
-e=None 
-# e is a none type value used to represent that there is nothing in the list/file 
-print(a,b,c,d,e)
+#example                           # Data types
+x = "Hello World"                  # string
+x = 20                             # integer
+x = 20.5                           # float 
+x = 1j                             # complex
+x = ["apple","banana"]             # list
+x = ("apple","banana")             # tuple
+x = range(5)                       # range
+x = {"name":"Aaryan","age":19}     # dictionary
+x = {"apple","banana"}             # set
+x = frozenset({"apple","mango"})   # frozenset
+x = True                           # boolean
+x = b"Hello"                       # bytes
+x = None                           # none Type
